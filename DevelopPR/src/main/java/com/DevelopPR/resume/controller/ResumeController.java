3b1949d05221d733bb602d9aab2c793b07ff9621
Soobinnn <Soobinnn@DESKTOP-3PV75D0>
2019-01-14 19:41:12 +0900

    src/main/java경로 못찾는것 수정

---
+++ DevelopPR/src/main/java/com/DevelopPR/resume/controller/ResumeController.java
@@ -0,0 +1,14 @@
+package com.DevelopPR.resume.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/resume/*")
+public class ResumeController {
+   @RequestMapping(value="regist", method=RequestMethod.GET)
+   public String resumeRegist() throws Exception{
+      return "resume/regist";
+   }
+}
