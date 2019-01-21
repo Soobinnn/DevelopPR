039ab10d23d3b5d800b7694bd4fb5124db18b102
Soobinnn <Soobinnn@DESKTOP-3PV75D0>
2019-01-21 17:01:26 +0900

    1.21 수빈 웹소켓추가 2

--- DevelopPR/src/main/java/com/DevelopPR/meet/util/WebSocket.java
+++ DevelopPR/src/main/java/com/DevelopPR/meet/util/WebSocket.java
@@ -1,6 +1,10 @@
 package com.DevelopPR.meet.util;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -9,6 +13,13 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class WebSocket extends TextWebSocketHandler 
 {
+
+	//서버에 연결된 사용자들을 저장하기위해 선언
+/*	private List<WebSocketSession> sessionList = new ArrayList<>(); //메세지를 날려주기위한 웹소켓전용 세션
+	private Map<WebSocketSession, String> mapList = new HashMap<>(); //실제session의 아이디정보, web소켓정보
+	private Map<WebSocketSession,String> roomList = new HashMap<>(); //실제 session의 아이디정보,  room정보
+	private List<String> userList = new ArrayList<>(); //접속자 명단을 개개인별로 뿌려주기위해 선언한 일반리스트
+*/
 	//연결되었을때
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception 
